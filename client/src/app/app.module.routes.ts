a578fc247f91646c2a55f9a8297c1e7efab2ca9f
pedro <pedrohlcastro@gmail.com>
2019-04-19 20:15:54 -0300

    Add SignIn Page

--- client/src/app/app.module.routes.ts
+++ client/src/app/app.module.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 const ROUTES: Routes = [
-    { path: '', component: HomePageComponent }
+    { path: '', component: HomePageComponent },
+    { path: 'sign-in', component: SignInComponent }
   ];
   
 @NgModule({
--- client/src/app/components/sign-in/sign-in.component.ts
+++ client/src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(50)
+    ])
+  });
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit() {
   }
+
+  login(){
+    if (this.loginForm.invalid){
+      this.snackBar.open("Não foi possível efetuar o login, favor tentar novamente.", 'Fechar', {duration: 3000});
+      return;
+    }
+
+    const requestUser = {
+      username: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+    this.authService.loginUser(requestUser)
+      .subscribe((res) => {
+        this.snackBar.open("Login efetuado com sucesso.", 'Fechar', {duration: 3000});
+      },
+      error => {
+        console.log(error.statusText);
+        this.snackBar.open("Não foi possível efetuar login, favor tentar novamente.", 'Fechar', {duration: 3000});
+      });
+  }
+
+
 
 }
