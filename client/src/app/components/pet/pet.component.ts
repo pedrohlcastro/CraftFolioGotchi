e8acef1f4fd557184a1cc2ce2c2f73055d573d66
pedro <pedrohlcastro@gmail.com>
2019-04-21 19:10:27 -0300

    Add pet

--- client/src/app/components/pet/pet.component.ts
+++ client/src/app/components/pet/pet.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+declare const $;
 
 @Component({
   selector: 'app-pet',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pet.component.scss']
 })
 export class PetComponent implements OnInit {
-
+  velocity = 0.5
+  position = 0
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  @HostListener('window:keydown', ['$event'])
+  move(ev){
+    $('#pet').removeClass('move-right move-left')
+    if (37 === ev.keyCode) {
+      $('#pet').addClass('move-left')
+      if((this.position - this.velocity) >= 0){
+        this.position -= this.velocity;
+      }
+    }
+    if (39 === ev.keyCode) {
+      $('#pet').addClass('move-right')
+      if((this.position + this.velocity) <= 80){
+        this.position += this.velocity;
+      }
+    }
+    $('#pet').css('margin-left', `${this.position}%`)
+  }
 }
